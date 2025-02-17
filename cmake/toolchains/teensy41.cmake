e9801dcf7c7b2f559b1b7a15fed395c2fd8048f0
newdigate <nicnewdigate@gmail.com>
2025-02-17 17:42:27 +0000

    minor corrections

--- cmake/toolchains/teensy41.cmake
+++ cmake/toolchains/teensy41.cmake
@@ -5,19 +5,10 @@ set(CMAKE_EXE_LINKER_FLAGS "--specs=nano.specs" CACHE INTERNAL "")
 set(COMPILERPATH "/opt/gcc-arm-none-eabi-10.3-2021.10/bin/")
 #set(COMPILERPATH "/Applications/ARM/bin/")
 
-add_definitions(-DTEENSY_VERSION=${TEENSY_VERSION})
-include(FetchContent)
-
-FetchContent_Declare(teensy_cores
-        GIT_REPOSITORY https://github.com/PaulStoffregen/cores
-        GIT_TAG        master
-)
-FetchContent_MakeAvailable(teensy_cores)
-
-FetchContent_Declare(teensy_cmake_macros
-        GIT_REPOSITORY https://github.com/newdigate/teensy-cmake-macros
-        GIT_TAG        noinstall
-)
-FetchContent_MakeAvailable(teensy_cmake_macros)
-include(${teensy_cmake_macros_SOURCE_DIR}/CMakeLists.txt)
 set(BUILD_FOR_TEENSY ON)
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY")
+set(CMAKE_C_COMPILER ${COMPILERPATH}arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER ${COMPILERPATH}arm-none-eabi-g++)
+set(CMAKE_CXX_LINK_EXECUTABLE "${CMAKE_C_COMPILER} <FLAGS> <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
